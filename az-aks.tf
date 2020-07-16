048db64817b1ce3a490c1018ffcee9121195ed8c
ashishpatel3 <66478966+ashishpatel3@users.noreply.github.com>
2020-07-16 14:01:47 +0100

    First Release

---
+++ az-aks.tf
@@ -0,0 +1,34 @@
+resource "azurerm_kubernetes_cluster" "demo" {
+    name                = "${var.aks_cluster_name}"
+    location            = "${azurerm_resource_group.aks.location}"
+    dns_prefix          = "ps-${var.aks_cluster_name}"
+    resource_group_name = "${azurerm_resource_group.aks.name}"
+
+    
+    default_node_pool {
+        name            = "nodepool01"
+        node_count           = "3"
+        vm_size         = "Standard_DS2_v2"
+        
+        os_disk_size_gb = 30
+
+        # Required for advanced networking
+        vnet_subnet_id = "${azurerm_subnet.cluster.id}"
+    }
+
+    service_principal {
+        client_id     = "${azuread_service_principal.sp-aks.application_id}"
+        client_secret = "${random_string.sp-aks-password.result}"
+    }
+
+    network_profile {
+        network_plugin = "azure"
+    }
+
+    addon_profile {
+        oms_agent {
+            enabled = true
+            log_analytics_workspace_id = "${azurerm_log_analytics_workspace.aks.id}"
+        }
+    }
+}
---
+++ provider.tf
@@ -0,0 +1,9 @@
+provider "azurerm" {
+  version = "~>2.18"
+  features {
+
+  }
+}
+provider "azuread" {
+  version = "~>0.11"
+}
